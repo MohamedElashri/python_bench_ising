f1f34b24f415061fe305491048d1837ac0fff079
Mohamed Elashri <muhammadelashri@gmail.com>
2023-07-22 19:53:15 -0400

    debug the plots part of the CI

--- generate_plots.py
+++ generate_plots.py
@@ -14,8 +14,6 @@ def parse_results(file):
             results["Average magnetization"] = float(line.split(':')[-1].strip())
         if "Average heat capacity" in line:
             results["Average heat capacity"] = float(line.split(':')[-1].strip())
-        # add more keys as needed
-    print(f"Parsed results: {results}")
     return results
 
 def plot_results(results, L, n):
@@ -28,25 +26,31 @@ def plot_results(results, L, n):
     plt.ylabel("Execution time (s)")
     plt.title(f"Execution time for different Python versions (L={L}, n={n})")
 
-    # Ensure the plots directory exists
-    os.makedirs("plots", exist_ok=True)
 
-    output_file = f"plots/time_L{L}_n{n}.png"
+    output_file = f"plots/"time_L{L}_n{n}.png"
     plt.savefig(output_file)
-
+    plt.close()  
     print(f"Plot saved to: {output_file}")  
 
 def main():
-    results = {}
-    for file in os.listdir('.'):
-        if file.startswith('results_') and file.endswith('.txt'):
-            parts = file.split('_')
-            python_version = parts[1].split('.')[0]
-            L = parts[2][1:]
-            n = parts[3].split('.')[0][1:]
+    result_files = [f for f in os.listdir('.') if f.startswith('results_') and f.endswith('.txt')]
+
+    # Group files by (L, n) combination
+    datasets = {}
+    for file in result_files:
+        parts = file.split('_')
+        dataset_key = (parts[2][1:], parts[3].split('.')[0][1:])
+        if dataset_key not in datasets:
+            datasets[dataset_key] = []
+        datasets[dataset_key].append(file)
+
+    # Parse results and generate plots for each (L, n) combination
+    for (L, n), files in datasets.items():
+        results = {}
+        for file in files:
+            python_version = file.split('_')[1]
             results[python_version] = parse_results(file)
-            plot_results(results, L, n)
-            results.clear()
+        plot_results(results, L, n)
 
 if __name__ == "__main__":
     main()
